b7526917c461e0190e1cf3f2fd94ca048111aa24
elkcityhazard <41174540+elkcityhazard@users.noreply.github.com>
2020-03-06 14:25:17 -0500

    added local storage feature
    added local storage feature to store seats and prices

--- script.js
+++ script.js
@@ -4,20 +4,56 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
+populateUI();
+
+
 let ticketPrice = +movieSelect.value; 
+
+        // Save selected movie index and price
+
+        function setMovieData(movieIndex, moviePrice) {
+            localStorage.setItem('selectedMovieIndex', movieIndex);
+            localStorage.setItem('selectedMoviePrice', moviePrice);
+        }
 
         //  Update total and count
 function updateSelectedCount() {
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
+    const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
     const selectedSeatsCount = selectedSeats.length;
     count.innerText = selectedSeatsCount;
     total.innerText = selectedSeatsCount * ticketPrice;
 }
 
+
+        //  Get Data from localStorage and populateUI
+
+        function populateUI() {
+            const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+            console.log(selectedSeats);
+            if (selectedSeats !== null && selectedSeats.length > 0) {
+                seats.forEach((seat, index) => {
+                    if(selectedSeats.indexOf(index) > -1) {
+                        seat.classList.add('selected');
+                    }
+                });
+            }
+
+            const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+            if (selectedMovieIndex !== null) {
+                movieSelect.selectedIndex = selectedMovieIndex;
+            }
+        }
+
+
+
+
 //  Movie Select Event
 movieSelect.addEventListener('change', e => {
     ticketPrice = +e.target.value;
-    updateSelectedCount();
+        updateSelectedCount();
 });
 
 //  Seat Click Event
@@ -28,3 +64,6 @@ container.addEventListener('click', (e) => {
         updateSelectedCount();
     }
 });
+
+//  Initial count and total set
+updateSelectedCount();
